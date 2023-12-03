eea53b11f5fbaccddbcc12adcf4a5f96c3313a27
Sadikibenda <Sadiki.benda1@gmail.com>
2023-12-02 23:15:05 -0500

    styel css

---
+++ DOM.js
@@ -0,0 +1,36 @@
+
+
+let button = document.getElementsByTagName('button')[0]
+let input = document.getElementById('input')
+let ul  =document.querySelector('ul')
+
+
+button.addEventListener('click', function() {
+
+    if (input.value.length > 0) {
+        let li = document.createElement('li')
+        let textnode = document.createTextNode(input.value)
+        li.appendChild(textnode)
+        ul.appendChild(li)
+        input.value = '';
+    } 
+})
+
+
+/ul.addEventListener('click', function(){
+    console.log('Azfar')
+    /*let item = document.getElementsByTagName(ul.value)
+    ul.removeChild(item)*/
+})
+
+
+/*let list = document.getElementById('demo')
+
+ function typeHere(){
+    let newitem = document.createElement('li')
+    let textnode = document.createTextNode(input)
+    let input = document.getElementsByClassName('input')[0].value;
+     
+     newitem.appendChild(textnode)
+     list.appendChild(newitem)
+ }*/
